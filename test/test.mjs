7834d45fa3d82733a310210ab5d4b9fe9ff12aa6
Alan Layng <alan@readersludlow.com>
2021-04-24 17:25:10 +0100

    s/ava/uvu/

--- test/test.mjs
+++ test/test.mjs
@@ -1,90 +1,101 @@
-import test from 'ava'
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+
 import promiseGoodies from 'promise-goodies'
+
 import PLock from '../src/index.mjs'
 
 promiseGoodies()
 
-test('acquire and release', async t => {
+test('acquire and release', async () => {
   const l = new PLock()
 
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
   await l.acquire()
 
-  t.is(l.count, 1)
+  assert.is(l.count, 1)
   l.release()
 
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
 })
 
-test('waiting for lock', async t => {
+test('waiting for lock', async () => {
   const l = new PLock()
   await l.acquire()
 
   const p1 = l.acquire()
-  t.false(await p1.isResolved())
+  assert.not.ok(await p1.isResolved())
 
   l.release()
-  t.is(l.count, 1)
-  t.true(await p1.isResolved())
+  assert.is(l.count, 1)
+  assert.ok(await p1.isResolved())
 
   l.release()
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
 })
 
-test('multiple width lock', async t => {
+test('multiple width lock', async () => {
   const l = new PLock({ width: 2 })
 
   await l.acquire()
-  t.is(l.count, 1)
+  assert.is(l.count, 1)
 
   await l.acquire()
-  t.is(l.count, 2)
+  assert.is(l.count, 2)
 
   const p3 = l.acquire()
-  t.is(l.waiting, 1)
-  t.false(await p3.isResolved())
+  assert.is(l.waiting, 1)
+  assert.not.ok(await p3.isResolved())
 
   l.release()
-  t.is(l.waiting, 0)
-  t.is(l.count, 2)
+  assert.is(l.waiting, 0)
+  assert.is(l.count, 2)
 
   l.release()
-  t.is(l.count, 1)
+  assert.is(l.count, 1)
 
   await l.acquire()
   l.release()
   l.release()
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
 })
 
-test('over-release lock', async t => {
+test('over-release lock', async () => {
   const l = new PLock()
   await l.acquire()
   l.release()
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
   l.release()
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
 })
 
-test('exec', async t => {
+test('exec', async () => {
   const l = new PLock()
   await l.acquire()
 
   const p = l.exec(() => 17)
-  t.false(await p.isResolved())
+  assert.not.ok(await p.isResolved())
 
   l.release()
-  t.true(await p.isResolved())
-  t.is(await p, 17)
+  assert.ok(await p.isResolved())
+  assert.is(await p, 17)
 
   const e = new Error('oops')
 
-  const err = await t.throwsAsync(() =>
-    l.exec(() => {
+  await l
+    .exec(() => {
       throw e
     })
-  )
+    .then(
+      () => {
+        assert.unreachable()
+      },
+      err => {
+        assert.is(err, e)
+      }
+    )
 
-  t.is(err, e)
-  t.is(l.count, 0)
+  assert.is(l.count, 0)
 })
+
+test.run()
